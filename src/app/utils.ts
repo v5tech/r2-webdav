fdd6a14044336042130e1eaba6d53a16ba9b4881
冯靖 <sxyx2008@163.com>
2026-05-23 20:08:09 +0800

    ♻️ refactor: drop 12 legacy MUI files [Phase 2b C2]

--- src/app/utils.ts
+++
@@ -1,9 +0,0 @@
-export function humanReadableSize(size: number) {
-  const units = ['B', 'KB', 'MB', 'GB', 'TB']
-  let i = 0
-  while (size >= 1024) {
-    size /= 1024
-    i++
-  }
-  return `${size.toFixed(1)} ${units[i]}`
-}
