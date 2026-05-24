f60d8aa9ee554a7759c3b1f6550335fefaaa6f11
冯靖 <sxyx2008@163.com>
2026-05-24 13:32:05 +0800

    🔒 chore(zod): hoist jitless config to early side-effect module (ESM order fix) [Phase 2b T2b.10]

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
+import './lib/zod-config'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { z } from 'zod'
 
 import App from './App'
 import './lib/i18n'
 import { initTheme } from './lib/theme'
 import './styles/globals.css'
 
-z.config({ jitless: true })
 initTheme()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
